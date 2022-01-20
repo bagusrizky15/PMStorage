20086cd0d947b3a8fff204858abd6f290fea1d19
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-01-20 15:29:05 +0700

    storage

--- app/src/main/java/com/rivvana/pm_storage/InStorageActivity.java
+++ app/src/main/java/com/rivvana/pm_storage/InStorageActivity.java
@@ -2,11 +2,20 @@ package com.rivvana.pm_storage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.appsearch.StorageInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class InStorageActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class InStorageActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String FILENAME = "myfile.txt";
     Button btnCreate , btnEdit, btnRead, btnDelete;
@@ -33,7 +42,80 @@ public class InStorageActivity extends AppCompatActivity {
     }
 
     void createFile(){
-        
+
+        String isiFile = "Hello";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, true);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
+    void editFile(){
+
+        String ubah = "Update Hello";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(ubah.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void readFile(){
+        File storage = getFilesDir();
+        File file = new File(storage, FILENAME);
+
+        if (file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+
+                while (line!=null){
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e){
+                System.out.println("Error " + e.getMessage());
+            }
+            tvRead.setText(text.toString());
+        }
+    }
+
+    void deleteFile (){
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()){
+            file.delete();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_create){
+            createFile();
+        }else if (id == R.id.btn_edit){
+            editFile();
+        }else if (id == R.id.btn_read){
+            readFile();
+        }else if (id == R.id.btn_delete){
+            deleteFile();
+        }
+    }
 }
